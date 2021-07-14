7cfddebfdbfeffe95b66dd09bc5474df1bb87a53
Wataru Tsukada <wataru.tsukada47@gmail.com>
2021-07-14 21:34:00 +0900

    AtCorder練習用レポジトリ初期コミット

---
+++ AtCoder/ABC209-A/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC209_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ AtCoder/ABC209-B/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ABC209_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
