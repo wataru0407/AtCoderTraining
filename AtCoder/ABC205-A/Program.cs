95c4b798494413f83cbbb0833980caa51fe502e4
Wataru Tsukada <wataru.tsukada47@gmail.com>
2021-07-27 10:07:44 +0900

    ABC205のA,B問題を解く

---
+++ AtCoder/ABC205-A/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC205_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            // 入力値に対するチェックを行い、不正であれば終了する
+            if (!IsValid(input, 2))
+            {
+                return;
+            }
+
+            var numbers = input.Split(" ").Select(x => int.Parse(x));
+
+            // 入力値の大小関係に対するチェックを行い、不正であれば終了する
+            if (numbers.Any(x => !IsValidNumber(0, 1000, x)))
+            {
+                return;
+            }
+
+            int cal = numbers.ElementAt(0);
+            int ml = numbers.ElementAt(1);
+            int unitCal = 100;
+
+            double result = ((double)cal * ml) / unitCal;
+
+            Console.WriteLine(result);
+        }
+
+        private static bool IsValid(string input, int length = 1)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("標準入力が空です。");
+                return false;
+            }
+
+            var split = input.Split(" ");
+
+            if (split.Length != length)
+            {
+                Console.WriteLine("標準入力の値の個数が不正です。");
+                return false;
+            }
+
+            if (!split.All(x => int.TryParse(x, out var y)))
+            {
+                Console.WriteLine("標準入力が整数ではありません。");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsValidNumber(int minValue, int MaxValue, int value)
+        {
+            if (!(minValue <= value && value <= MaxValue))
+            {
+                Console.WriteLine("標準入力が制約の大小関係を満たしていません。");
+                return false;
+
+            }
+            return true;
+        }
+    }
+}
