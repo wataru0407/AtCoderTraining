076241ee8bc7d41bfc5dca84e5e7cb2485a49aa1
wataru0407 <wataru.tsukada47@gmail.com>
2021-07-28 11:35:43 +0900

    ABC204を解く

---
+++ AtCoder/ABC204-A/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC204_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            // 入力値に対するチェックを行い、不正であれば終了する
+            if (!IsValid(input, 2))
+            {
+                return;
+            }
+
+            var numbers = input.Split(" ").Select(x => int.Parse(x));
+
+            // 入力値の大小関係に対するチェックを行い、不正であれば終了する
+            if (numbers.Any(x => !IsValidNumber(0, 2, x)))
+            {
+                return;
+            }
+
+            int x = numbers.ElementAt(0);
+            int y = numbers.ElementAt(1);
+
+            // 2人が同じ手の場合は同じ手、異なる場合は残りの手とする
+            int result = x == y ? x : 3 - (x + y);
+
+            Console.WriteLine(result);
+        }
+
+        private static bool IsValid(string input, int length = 1)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("標準入力が空です。");
+                return false;
+            }
+
+            var split = input.Split(" ");
+
+            if (split.Length != length)
+            {
+                Console.WriteLine("標準入力の値の個数が不正です。");
+                return false;
+            }
+
+            if (!split.All(x => int.TryParse(x, out var y)))
+            {
+                Console.WriteLine("標準入力が整数ではありません。");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsValidNumber(int minValue, int MaxValue, int value)
+        {
+            if (!(minValue <= value && value <= MaxValue))
+            {
+                Console.WriteLine("標準入力が制約の大小関係を満たしていません。");
+                return false;
+
+            }
+            return true;
+        }
+
+    }
+}
---
+++ AtCoder/ABC204-B/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq;
+
+namespace ABC204_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1行目の読み込み
+            var input1 = Console.ReadLine();
+
+            // 入力値に対するチェックを行い、不正であれば終了する
+            if (!IsValid(input1, 1))
+            {
+                return;
+            }
+
+            var number = int.Parse(input1);
+
+            // 入力値の大小関係に対するチェックを行い、不正であれば終了する
+            if (!IsValidNumber(1, 1000, number))
+            {
+                return;
+            }
+
+            //2行目の読み込み
+            var input2 = Console.ReadLine();
+
+            // 入力値に対するチェックを行い、不正であれば終了する
+            if (!IsValid(input2, number))
+            {
+                return;
+            }
+
+            var nuts = input2.Split(" ").Select(x => int.Parse(x));
+
+            // 入力値の大小関係に対するチェックを行い、不正であれば終了する
+            if (nuts.Any(x => !IsValidNumber(0, 1000, x)))
+            {
+                return;
+            }
+
+            //　収穫する木の実の数を計算する
+            var result = nuts.Select(x => x > 10 ? x - 10 : 0).Sum();
+
+            Console.WriteLine(result);
+        }
+
+        private static bool IsValid(string input, int? length = null)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("標準入力が空です。");
+                return false;
+            }
+
+            var split = input.Split(" ");
+
+            if (length.HasValue && split.Length != length)
+            {
+                Console.WriteLine("標準入力の値の個数が不正です。");
+                return false;
+            }
+
+            if (!split.All(x => int.TryParse(x, out var y)))
+            {
+                Console.WriteLine("標準入力が整数ではありません。");
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsValidNumber(int minValue, int MaxValue, int value)
+        {
+            if (!(minValue <= value && value <= MaxValue))
+            {
+                Console.WriteLine("標準入力が制約の大小関係を満たしていません。");
+                return false;
+
+            }
+            return true;
+        }
+    }
+}
