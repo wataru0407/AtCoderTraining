d0f51fedce09ddd2241711bd3dae9c6097cce46d
Wataru Tsukada <wataru.tsukada47@gmail.com>
2021-08-26 02:21:18 +0900

    LeetCode104を解く

---
+++ LeetCode/104.MaximumDepthOfBinaryTree/Solution.cs
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public int MaxDepth(TreeNode root) {
+        return Dfs(root);
+    }
+
+    private int Dfs(TreeNode root)
+    {
+        // ノードが空の場合は0を返す
+        if (root == null)
+        {
+            return 0;
+        }
+
+        // 左右ノードの最大値に1を足した数を再帰的に計算する
+        return (Math.Max(Dfs(root.left), Dfs(root.left)) + 1;
+    }
+}
