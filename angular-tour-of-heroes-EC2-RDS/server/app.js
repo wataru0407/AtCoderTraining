0ea66fcac17c17b1670306b72290cb0a45c89ca6
wataru0407 <wataru.tsukada47@gmail.com>
2021-09-07 09:58:23 +0900

    RDS用に一旦修正

--- angular-tour-of-heroes-EC2-RDS/server/app.js
+++ angular-tour-of-heroes-EC2-RDS/server/app.js
@@ -1,33 +1,64 @@
 const express = require('express');
+const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+const connection = mysql.createConnection({
+  host: '',
+  //host: 'localhost',
+  user: 'admin',
+  //user: 'root',
+  password: '',
+  database: ''
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
-const heroes = [
-    { id: 11, name: 'Dr Nice' },
-    { id: 12, name: 'Narco' },
-    { id: 13, name: 'Bombasto' },
-    { id: 14, name: 'Celeritas' },
-    { id: 15, name: 'Magneta' },
-    { id: 16, name: 'RubberMan' },
-    { id: 17, name: 'Dynama' },
-    { id: 18, name: 'Dr IQ' },
-    { id: 19, name: 'Magma' },
-    { id: 20, name: 'Tornado' },
-];
+// const heroes = [
+//     { id: 11, name: 'Dr Nice' },
+//     { id: 12, name: 'Narco' },
+//     { id: 13, name: 'Bombasto' },
+//     { id: 14, name: 'Celeritas' },
+//     { id: 15, name: 'Magneta' },
+//     { id: 16, name: 'RubberMan' },
+//     { id: 17, name: 'Dynama' },
+//     { id: 18, name: 'Dr IQ' },
+//     { id: 19, name: 'Magma' },
+//     { id: 20, name: 'Tornado' },
+// ];
 
-app.get("/api/heroes", function(req, res, next){
-    res.json(heroes);
+app.get("/api/heroes", function(req, res, ){
+    // res.json(heroes);
+    connection.query(
+      'SELECT * FROM angular_tutorial.heroes',
+      (error, results) => {
+        console.log("/api/heroes");
+        console.log(results);
+        res.json(results);
+      }
+    );
 });
 
 app.get("/api/heroes/:id", function(req, res, next){
-    const hero = heroes.find(x => x.id == req.params.id);
-    res.json(hero);
+    //const hero = heroes.find(x => x.id == req.params.id);
+    connection.query(
+      `SELECT * FROM angular_tutorial.heroes where id = ${req.params.id}`,
+      (error, results) => {
+        console.log("/api/heroes/:id");
+        console.log(results);
+        const hero = results[0];
+        res.json(hero);
+      }
+    );
+    //res.json(hero);
 });
