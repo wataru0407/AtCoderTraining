88546e473b2c09b54e53b345510165be4dad1a78
wataru0407 <wataru.tsukada47@gmail.com>
2021-09-27 08:33:54 +0900

    SQL修正

--- angular-tour-of-heroes-EC2-RDS/server/app.js
+++ angular-tour-of-heroes-EC2-RDS/server/app.js
@@ -1,15 +1,20 @@
+const e = require('express');
 const express = require('express');
+
 const mysql = require('mysql');
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
+require('dotenv').config();
+
 const connection = mysql.createConnection({
-  host: '',
-  //host: 'localhost',
-  user: 'admin',
-  //user: 'root',
-  password: '',
-  database: ''
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 connection.connect((err) => {
@@ -24,27 +29,19 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 });
 
-// const heroes = [
-//     { id: 11, name: 'Dr Nice' },
-//     { id: 12, name: 'Narco' },
-//     { id: 13, name: 'Bombasto' },
-//     { id: 14, name: 'Celeritas' },
-//     { id: 15, name: 'Magneta' },
-//     { id: 16, name: 'RubberMan' },
-//     { id: 17, name: 'Dynama' },
-//     { id: 18, name: 'Dr IQ' },
-//     { id: 19, name: 'Magma' },
-//     { id: 20, name: 'Tornado' },
-// ];
-
-app.get("/api/heroes", function(req, res, ){
-    // res.json(heroes);
+app.get("/api/heroes", function(req, res, next){
     connection.query(
-      'SELECT * FROM angular_tutorial.heroes',
+      'SELECT * FROM heroes',
       (error, results) => {
         console.log("/api/heroes");
         console.log(results);
-        res.json(results);
+        const searchName = req.query.name;
+        if (searchName) {
+          const rec = results.filter(item => item.name.includes(searchName));
+          res.json(rec);
+        } else {
+          res.json(results);
+        }
       }
     );
 });
@@ -52,7 +49,7 @@ app.get("/api/heroes", function(req, res, ){
 app.get("/api/heroes/:id", function(req, res, next){
     //const hero = heroes.find(x => x.id == req.params.id);
     connection.query(
-      `SELECT * FROM angular_tutorial.heroes where id = ${req.params.id}`,
+      `SELECT * FROM heroes where id = ${req.params.id}`,
       (error, results) => {
         console.log("/api/heroes/:id");
         console.log(results);
@@ -62,3 +59,101 @@ app.get("/api/heroes/:id", function(req, res, next){
     );
     //res.json(hero);
 });
+
+app.get("/api/heroes/:id", function(req, res, next){
+  //const hero = heroes.find(x => x.id == req.params.id);
+  connection.query(
+    `SELECT * FROM heroes where id = ${req.params.id}`,
+    (error, results) => {
+      console.log("/api/heroes/:id");
+      console.log(results);
+      const hero = results[0];
+      res.json(hero);
+    }
+  );
+  //res.json(hero);
+});
+
+// app.put("/api/heroes", function(req, res, next){
+//   //const hero = heroes.find(x => x.id == req.params.id);
+//   connection.query(
+//     `UPDATE heroes set name = '${req.params.name}' where id = ${req.params.id}`,
+//     `SELECT * FROM angular_tutorial.heroes where id = ${req.params.id}`,
+//     (error, results) => {
+//       console.log(results);
+//       const hero = results[0];
+//       res.json(hero);
+//     }
+//   );
+//   //res.json(hero);
+// });
+
+app.put("/api/heroes", (req, res) => {
+  console.log("post start");
+  const id = req.body.id;
+  const name = req.body.name;
+  console.log(name);
+  const sql = `UPDATE heroes SET name = '${name}' where id = ${id}`;
+  console.log(sql);
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    //const insertId = result.insertId;
+    //const resHero = {id: insertId, name: name};
+    //res.json(resHero);
+  })
+});
+
+
+app.post("/api/heroes", (req, res) => {
+  console.log("put start");
+  const name = req.body.name;
+  console.log(name);
+  const sql = `INSERT INTO heroes(name) VALUES('${name}')`;
+  console.log(sql);
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    const insertId = result.insertId;
+    const resHero = {id: insertId, name: name};
+    res.json(resHero);
+  })
+});
+
+app.delete("/api/heroes/:id", (req, res) => {
+  console.log("delete start");
+  const deleteId = req.params.id;
+  const sql = `DELETE FROM heroes where id = ${deleteId}`;
+  console.log(sql);
+  connection.query(sql, (err, result) => {
+    if (err) throw err;
+    console.log(result);
+    // const insertId = result.insertId;
+    // const resHero = {id: insertId, name: name};
+    // res.json(resHero);
+  })
+});
+
+
+// app.post("/api/heroes", function(req, res, next){
+//   //const hero = heroes.find(x => x.id == req.params.id);
+//   // const sql = `INSERT INTO heroes VALUES('${req.name}')`;
+//   const sql = `INSERT INTO heroes SET ?`;
+
+//   console.log(req.body);
+//   connection.query(sql, function(err, result, fields){
+//     if (err) throw err;
+//     console.log(result);
+//     res.send('完了');
+//   })
+//   // console.log(sql);
+//   // connection.query(
+//   //   `INSERT INTO heroes VALUES('${req.params.name}')`,
+//   //   (error, results) => {
+//   //     console.log(results);
+//   //     const hero = results[0];
+//   //     res.json(hero);
+//   //   }
+//   // );
+//   //res.json(hero);
+// });
